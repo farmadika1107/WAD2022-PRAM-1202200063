edaa4b39707790f5adb5bf4736342803d76cf381
pramudyafarmadika <pramfrmdika77@gmail.com>
2022-10-30 21:13:05 +0700

    tp2

---
+++ MODUL2 PRAMUDYA/home.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+     <title>Home</title>
+</head>
+
+<body>
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <div class="collapse navbar-collapse d-flex justify-content-center">
+               <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="home.php">Home</a>
+                    <a class="nav-item nav-link" href="book.php">Booking</a>
+               </div>
+          </div>
+     </nav>
+
+     <div class="title text-center">
+          <h4 class="text-info mt-3 text-dark">WELCOME TO EAD RENT </h4>
+          
+     </div>
+
+     <div class="container d-flex justify-content-center p-2 mt-5">
+
+          <div class="card mx-5">
+               <img class="card-img-top"
+                    src="toyotaford.jpeg">
+               <div class="card-body">
+               <h4 class="card-title text-left">Toyota Ford<br>
+               <h5><span class="text-muted">Rp 300.000/day</span><br>
+                    <p class="card-text">
+                         <table class="table text-center">
+                              <tr>
+                                   <td>7 Kursi</td>
+                              </tr>
+                              <tr>
+                                   <td>1500 cc</td>
+                              </tr>
+                              <tr>
+                                   <td>AUTOMATIC</td>
+                              </tr>
+                         </table>
+                    </p>
+               </div>
+                    <div class="card-footer d-flex justify-content-center">
+                         <a class="btn btn-primary" href="Booking.php?car=Toyota Ford">Book Now</a>
+                    </div>
+          </div>
+
+          <div class="card mx-5">
+               <img class="card-img-top"
+                    src="civic2022.png">
+               <div class="card-body">
+               <h4 class="card-title text-left">Civic<br>
+               <h5><span class="text-muted">Rp 350.000/day</span><br>
+                    <p class="card-text">
+                         <table class="table text-center">
+                              <tr>
+                                   <td>5 Kursi</td>
+                              </tr>
+                              <tr>
+                                   <td> 1500 CC</td>
+                              </tr>
+                              <tr>
+                                   <td> AUTOMATIC</td>
+                              </tr>
+                            
+                         </table>
+                    </p>
+               </div>
+               <div class="card-footer d-flex justify-content-center">
+                    <a class="btn btn-primary" href="Booking.php?car=Civic">Book Now</a>
+               </div>
+          </div>
+
+          <div class="card  mx-5">
+               <img class="card-img-top"
+                    src="xpander21.jpg">
+               <div class="card-body">
+               <h4 class="card-title text-left">Xpander<br>
+               <h5><span class="text-muted">Rp 280.000/day</span><br>
+                    <p class="card-text">
+                         <table class="table text-center">
+                              <tr>
+                                   <td>7 Kursi</td>
+                              </tr>
+                              <tr>
+                                   <td>1500 CC</td>
+                              </tr>
+                              <tr>
+                                   <td>AUTOMATIC</td>
+                              </tr>
+                         </table>
+                    </p>
+               </div>
+               <div class="card-footer d-flex justify-content-center">
+                    <a class="btn btn-primary" href="Booking.php?car=Xpander">Book Now</a>
+               </div>
+          </div>
+     </div>
+     <div class="card-footer text-muted text-center">
+    Created by : Pram_1202200063
+  </div>
+</div>
+</body>
+
+</html>
